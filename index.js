5bd936cc7cdbd941d7e10ede17a158d729462dbc
guilhermeepaixaoo <guilherme.paixao@mtistudio.com>
2016-01-24 06:11:15 -0200

    Resolvendo o algoritmo

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 
 /*
  * Algoritmo
- * [] Lista de todos os participantes
- * [] Lista de projetos abertos
- * [] Relacionar os Lista
- * [] Printar todos os projeto - participante
+ * [x] Lista de todos os participantes
+ * [x] Lista de projetos abertos
+ * [x] Relacionar os Lista
+ * [x] Printar todos os projeto - participante
  */
 
+var participant = [
+    {
+        name: 'Ronalson'
+    },
+    {
+        name: 'Diego'
+    },
+    {
+        name: 'Cayo'
+    },
+    {
+        name: 'Hugo'
+    },
+    {
+        name: 'Guilherme'
+    }
+];
+
+var opensource = [
+    {
+        name: 'primeiro'
+    },
+    {
+        name: 'segundo'
+    },
+    {
+        name: 'terceiro'
+    },
+    {
+        name: 'quarto'
+    },
+    {
+        name: 'quinto'
+    }
+];
+
+opensource.forEach(function( el, idx, arr ){
+
+    el[ 'author' ] = participant[ idx ].name;
+
+    console.log( "Project: " + el['name'], "Author: " + el['author'] );
+
+});
